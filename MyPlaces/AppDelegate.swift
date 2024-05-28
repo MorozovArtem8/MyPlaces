c5cb9036c8dded6b5df3058dec2c7b7052011927
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-05-28 21:04:59 +0300

    create

--- MyPlaces/AppDelegate.swift
+++ MyPlaces/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("applicationDidFinishLaunching")
+        return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 
