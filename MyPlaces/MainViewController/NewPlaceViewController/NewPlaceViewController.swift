4d99379d8b6f1e2d94cc18ff2ba2949eebb55900
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-06-04 21:58:01 +0300

    add, dell, swap for realm db actions

--- MyPlaces/MainViewController/NewPlaceViewController/NewPlaceViewController.swift
+++ MyPlaces/MainViewController/NewPlaceViewController/NewPlaceViewController.swift
@@ -4,6 +4,8 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
     
     var imageIsChanged = false
     
+    var currentPlace: Place?
+    
     @IBOutlet weak var placeImage: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var placeNameField: UITextField!
@@ -17,9 +19,10 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
         saveButton.isEnabled = false
         
         placeNameField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        setupEditScreen()
     }
     
-    func saveNewPlace() {
+    func savePlace() {
         
        
         var image: UIImage?
@@ -33,8 +36,41 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
         
         let newPlace = Place(name: placeNameField.text!, location: placeLocationField.text, type: placeTypeField.text, imageData: imageData)
         
-        
-        StorageManager.saveObject(newPlace)
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+            StorageManager.saveObject(newPlace)
+        }
+    }
+    
+    private func setupEditScreen() {
+        if currentPlace != nil {
+            guard let data = currentPlace?.imageData,
+                  let image = UIImage(data: data)
+            else {return}
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
+            placeNameField.text = currentPlace?.name
+            placeLocationField.text = currentPlace?.location
+            placeTypeField.text = currentPlace?.type
+            imageIsChanged = true
+            setupNavigationBar()
+        }
+    }
+    
+    func setupNavigationBar() {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
     }
     
     
